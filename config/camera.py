44e8667ebf56d3933f01d4961fcd157a9b88341e
kajuliaa <julia.kaidashova5@gmail.com>
2025-08-05 17:01:11 +0300

    changes to start

--- config/camera.py
+++ config/camera.py
@@ -4,5 +4,5 @@ from config.base import BaseConfig
 class CameraConfig(BaseConfig):
     name: str
     video_path: str
-    max_duration_seconds: int = 26
+    max_duration_seconds: int = 10
     detection_interval: int = 10  # Process detection every N frames
--- reindetification.py
+++ reindetification.py
@@ -1,21 +1,23 @@
 from ai_services.multicamera import MultiCameraTracker
 from config.camera import CameraConfig
-
+import time
 def main():
     """Main function to run the tracking system."""
     # Configuration
     reid_model_path = 'models/osnet_x1_0_market_256x128_amsgrad_ep150_stp60_lr0.0015_b64_fb10_softmax_labelsmooth_flip.pth'
 
     camera_configs = [
-        CameraConfig(name="cam1", video_path="videos/enter.mp4"),
-        CameraConfig(name="cam2", video_path="videos/elevator.mp4"),
-        CameraConfig(name="cam3", video_path="videos/arka.mp4"),
+        #CameraConfig(name="cam1", video_path="videos/enter.mp4"),
+        #CameraConfig(name="cam2", video_path="videos/elevator.mp4"),
+        CameraConfig(name="cam3", video_path="videos/arch_7am.mp4"),
     ]
 
-
+    start_time = time.time()
     # Initialize and run tracker
     tracker = MultiCameraTracker(reid_model_path, camera_configs)
     tracker.run()
+    end_time = time.time()
+    print(f'result time {end_time-start_time:.2f}')
 
 
 if __name__ == "__main__":
