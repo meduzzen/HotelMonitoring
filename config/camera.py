7fc9fdcd8c61d512c15d5fb1f25796d6f86a024d
kajuliaa <julia.kaidashova5@gmail.com>
2025-08-08 15:17:49 +0300

    fixed the video_path parameter

--- config/camera.py
+++ config/camera.py
@@ -1,9 +1,9 @@
 from config.base import BaseConfig
-
+from typing import Optional
 
 class CameraConfig(BaseConfig):
     name: str
-    video_path: str = None
+    video_path: Optional[str] = None
     stream_url: str = None
     max_duration_seconds: int = 60
     detection_interval: int = 400  # Process detection every N frames
