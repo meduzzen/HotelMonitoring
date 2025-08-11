c38be13779f82a8bb7e8917ec6ce5febe33a326a
kajuliaa <julia.kaidashova5@gmail.com>
2025-08-11 16:18:05 +0300

    added data_collection script

--- config/camera.py
+++ config/camera.py
@@ -4,5 +4,5 @@ from config.base import BaseConfig
 class CameraConfig(BaseConfig):
     name: str
     video_path: str
-    max_duration_seconds: int = 12
-    detection_interval: int = 96  # Process detection every N frames
+    max_duration_seconds: int = 4500
+    detection_interval: int = 24  # Process detection every N frames
